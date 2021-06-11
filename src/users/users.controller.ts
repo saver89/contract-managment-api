63734548ee26034b35d5d31fb09bd36158d27b8b
neelkim <neelkim1989@gmail.com>
2021-06-11 16:31:24 +0500

    feat: добавить выдачу роли, блокировку пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,8 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Roles } from 'src/auth/roles.auth.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BlockUserDto } from './dto/block-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -21,10 +23,28 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Получить всех пользователей' })
   @ApiResponse({ status: 200, type: [User] })
-  @Roles('USER')
+  @Roles('ADMIN')
   @UseGuards(RolesGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
+  }
+
+  @ApiOperation({ summary: 'Выдать роль' })
+  @ApiResponse({ status: 200 })
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  addRole(@Body() roleDto: AddRoleDto) {
+    return this.usersService.addRole(roleDto);
+  }
+
+  @ApiOperation({ summary: 'Заблокировать пользователя' })
+  @ApiResponse({ status: 200 })
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Post('/block')
+  block(@Body() dto: BlockUserDto) {
+    return this.usersService.block(dto);
   }
 }
