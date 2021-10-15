cf97e1883b48fac6100fc50cd1f4b7324f4e9366
Vladimir <neelkim1989@gmail.com>
2021-10-15 08:30:26 +0500

    feat: for frontend test add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: ['http://localhost:3000'] });
+
   const config = new DocumentBuilder()
     .setTitle('Биллинг договоров')
     .setDescription('Документация апи')
